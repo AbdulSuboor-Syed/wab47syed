a4766804adaa537d2f997ee3352b298ea052a03a
Syed, Abdul Suboor <77645775+AbdulSuboor-Syed@users.noreply.github.com>
2021-03-31 00:19:46 -0500

    updating stars

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var inkRouter = require('./routes/ink');
+var starsRouter = require('./routes/stars');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/ink', inkRouter);
+app.use('/stars', starsRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
